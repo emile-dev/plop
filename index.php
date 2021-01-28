7ff29c7cd91280403c56e0269a61497154bbe638
Emile <emile.seigneurin@hotmail.fr>
2021-01-28 15:00:09 +0100

    2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require('vendor/autoload.php');
 // $_SERVER['REQUEST_URI]
 // Si mon url != "plop-slack.herokuapp.com"
 
-if($_SERVER['HTTPS_HOST'] != "plop-slack.herokuapp.com"){
+if($_SERVER['HTTP_HOST'] != "plop-slack.herokuapp.com"){
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 }
