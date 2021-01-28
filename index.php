199d68ec449ce3f0fcb15c477b7872e2e1cc05cd
Emile <emile.seigneurin@hotmail.fr>
2021-01-28 14:57:26 +0100

    1

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo('Hello World');
+require('vendor/autoload.php');
+// $_SERVER['REQUEST_URI]
+// Si mon url != "plop-slack.herokuapp.com"
+
+if($_SERVER['HTTPS_HOST'] != "plop-slack.herokuapp.com"){
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
+
+// Sinon je ne le fais
+$ary = array("token" => $_ENV['TOKEN']);
+$data = json_encode($ary);
+//[{data: "Hello World}]
+
+echo($data);
